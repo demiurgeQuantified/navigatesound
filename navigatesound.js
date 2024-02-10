26ba844185ad2ff06c4f86e5b9a58aafc6e91634
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-02-10 09:46:01 +0000

    fix repeat sounds on some websites

--- navigatesound.js
+++ navigatesound.js
@@ -1,18 +1,21 @@
 let playing = false
 
-function playSound() {
-  if (!playing) {
-    playing = true
-    chrome.offscreen.createDocument({
-      url: chrome.runtime.getURL("playSound.html"),
-      reasons: ["AUDIO_PLAYBACK"],
-      justification: "navigate sound"
-    })
-    setTimeout(() => {
-      chrome.offscreen.closeDocument()
-      playing = false
-    }, 1000)
-  }
+function playSound(details) {
+  if (details.url == "about:blank" || details.url.startsWith("https://accounts.youtube.com/RotateCookiesPage?")) return
+
+  if (playing) return
+  playing = true
+
+  chrome.offscreen.createDocument({
+    url: chrome.runtime.getURL("playSound.html"),
+    reasons: ["AUDIO_PLAYBACK"],
+    justification: "navigate sound"
+  })
+
+  setTimeout(() => {
+    chrome.offscreen.closeDocument()
+    playing = false
+  }, 1000)
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(playSound)
