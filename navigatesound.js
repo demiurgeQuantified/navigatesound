2c25fec3a2dca2f2d973985145fc1ecd1c4a0965
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2024-02-10 09:09:07 +0000

    lock during timeout

--- navigatesound.js
+++ navigatesound.js
@@ -1,11 +1,18 @@
+let playing = false
+
 function playSound() {
-  console.log("Playing sound...")
-  chrome.offscreen.createDocument({
-    url: chrome.runtime.getURL("playSound.html"),
-    reasons: ["AUDIO_PLAYBACK"],
-    justification: "navigate sound"
-  })
-  setTimeout(chrome.offscreen.closeDocument, 1000)
+  if (!playing) {
+    playing = true
+    chrome.offscreen.createDocument({
+      url: chrome.runtime.getURL("playSound.html"),
+      reasons: ["AUDIO_PLAYBACK"],
+      justification: "navigate sound"
+    })
+    setTimeout(() => {
+      chrome.offscreen.closeDocument()
+      playing = false
+    }, 1000)
+  }
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(playSound)
